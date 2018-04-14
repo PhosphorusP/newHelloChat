bc7775d88093e4ca935209dd1756a9354d18dafe
PhosphorusP <1309396166@qq.com>
2018-04-14 14:54:18 +0800

    Comments

--- public/js/client.js
+++ public/js/client.js
@@ -11,6 +11,7 @@ window.onload = function() {
     initMessages();
 }
 
+//连接到服务器
 var socket = io.connect('/'); 
 socket.on('connect', function () {
     console.log('%cConnected to the server successfully.','color: #080');
@@ -22,15 +23,19 @@ socket.on('connect', function () {
         console.log('Hash of your connection is: %c' + data.socketID + '.', 'color: #00F');
         console.log('Hash of your client is: %c' + data.fromHash + '.', 'color: #00F');
         hideLogin();
-    }); 
+    });
+    //登录失败
     socket.on('loginFailed', function(data) {
         switch(data.reason) {
+            //昵称为空
             case 'nickEmpty': 
                 document.querySelector('.login .container .errormsg').innerHTML = '请输入昵称';
                 break;
+            //昵称过长
             case 'nickLong': 
                 document.querySelector('.login .container .errormsg').innerHTML = '昵称超过长度限制';
                 break;
+            //昵称被占用
             case 'nickExisted': 
                 document.querySelector('.login .container .errormsg').innerHTML = '昵称已被占用';
                 break;
@@ -38,14 +43,12 @@ socket.on('connect', function () {
     }); 
 }); 
 
+//初始化Toast通知Interval
 function initToasts() {
     setInterval('refreshToast()',3000);
 }
 
-function initMessages() {
-    setInterval('refreshMessageList()',200);
-}
-
+//更新Toast通知
 function refreshToast() {
     if(toasts.length>0) {
         showToast(toasts[0]);
@@ -53,6 +56,12 @@ function refreshToast() {
     }
 }
 
+//初始化消息列表
+function initMessages() {
+    setInterval('refreshMessageList()',200);
+}
+
+//向消息列表推送新消息
 function pushMlist(message, uuid) {
     for(i=0;i<mList.length;i++) {
         if(mList[i].uuid == uuid) {
@@ -67,6 +76,7 @@ function pushMlist(message, uuid) {
     refreshMlist();
 }
 
+//更新消息列表
 function refreshMlist() {
     for(i=0;i<mList.length;i++) {
         if(document.querySelector('.sidebar #dlg' + mList[i].uuid)==null) {
@@ -77,6 +87,7 @@ function refreshMlist() {
     }
 }
 
+//更新消息列表样式
 function refreshMessageList() {
     if(document.querySelectorAll('.sidebar .newdialog').length==1) document.querySelectorAll('.sidebar .newdialog')[0].setAttribute('class','dialog selected');
     for(i=0;i<document.querySelectorAll('.sidebar .newdialog').length;i++) {
@@ -87,6 +98,7 @@ function refreshMessageList() {
     }
 }
 
+//用户点击消息列表会话
 function selectDialog(obj) {
     if(obj == null) return false;
     for(i=0;i<document.querySelectorAll('dialog').length;i++) {
